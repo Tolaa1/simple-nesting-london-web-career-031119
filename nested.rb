4798d9c1e8c7d723160ef77ad2578098f2ada0a4
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-03-05 01:09:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,10 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      } 
-     alans_new_info = "GUI"
-     
-     programmer_hash[:alan_kay][:known_for] = alans_new_info
-     programmer_hash
+  
   
 end
 
