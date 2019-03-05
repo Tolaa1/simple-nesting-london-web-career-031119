6881c7782576e489c587499a647c7ec2ca1fd66f
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-03-05 01:06:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,8 +16,7 @@ def hopper
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-     programmer_hash
+
 end
 
 def alan_kay_is_known_for
